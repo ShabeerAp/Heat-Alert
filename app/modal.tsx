3fd377a2a123fe107b89a3ffdf3dc4eb60f398f3
ShabeerAp <msap916@gmail.com>
2025-07-22 00:48:18 +0400

    Update modal.tsx

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,9 +1,32 @@
-import { Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Platform, StyleSheet, Text, View } from "react-native";
 
-export default function Modal() {
+export default function ModalScreen() {
   return (
-    <View style={{ flex: 1, padding: 20 }}>
-      <Text>Settings or Alert Modal</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Modal</Text>
+      <View style={styles.separator} />
+      <Text>This is an example modal. You can edit it in app/modal.tsx.</Text>
+
+      {/* Use a light status bar on iOS to account for the black space above the modal */}
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
